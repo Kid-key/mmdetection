091a4121dc3b4ed11a3e60476ca7d4aca4475e87
Kid-key <fw.key@qq.com>
2021-03-15 15:00:33 +0800

    Update faster_rcnn_r50_fpn_1x_coco.py

--- configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py
+++ configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py
@@ -3,3 +3,5 @@ _base_ = [
     '../_base_/datasets/coco_detection.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
+log_config = dict(interval=1000)
+optimizer = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0001)
